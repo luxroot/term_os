e8b74a3cac0c82e2e70d0de53d183e4c69ba4fcc
m43ng <shinmg2520@gmail.com>
2019-04-30 02:30:00 +0000

    initial commit

---
+++ process.c
@@ -0,0 +1,5 @@
+#include "process.h"
+
+void pContainer_init(PContainer* pc_ptr){
+    pc_ptr->
+}
---
+++ process.h
@@ -0,0 +1,20 @@
+
+typedef uint unsigned int;
+
+typedef struct _Process{
+    uint pid;
+    uint cpu_burst;
+    uint io_burst;
+    uint arrival;
+    uint priority;
+} Process;
+
+typedef struct _PContainer{
+    Process* proc;
+    uint waiting_time;
+    uint done_time;
+    uint bursted;
+    uint quantum;
+} PContainer;
+
+void pContainer_init(PContainer* pc_ptr);
