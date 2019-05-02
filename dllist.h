a30195443b9ccd114fd008488f9332b0205b355f
m43ng <shinmg2520@gmail.com>
2019-05-03 02:37:42 +0900

    DL list header order modified

--- dllist.h
+++ dllist.h
@@ -11,27 +11,28 @@ typedef struct _Node{
 
 typedef Node *NodePtr;
 
+
 typedef struct _DLList{
     Node nil;
 }DLList;
 
 typedef DLList *DLLptr;
 
+
 void DLList_init(DLLptr);
+unsigned int get_size(DLLptr);
+
 void push_back(DLLptr dll, NodePtr nptr);
 void push_front(DLLptr, NodePtr);
 void push_after(NodePtr, NodePtr);
-
-unsigned int get_size(DLLptr);
-
-NodePtr pop_front(DLLptr);
-NodePtr pop_back(DLLptr);
-NodePtr pop_this(NodePtr);
-NodePtr pop_nth(DLLptr, unsigned int);
 
 NodePtr get_front(DLLptr);
 NodePtr get_back(DLLptr);
 NodePtr get_nth(DLLptr, unsigned int);
 
+NodePtr pop_front(DLLptr);
+NodePtr pop_back(DLLptr);
+NodePtr pop_this(NodePtr);
+NodePtr pop_nth(DLLptr, unsigned int);
 
 #endif //TERM_DLLIST_H
