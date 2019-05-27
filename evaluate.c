3fcfb7f933041d8e0af9bc6dcd13759ee9f54b65
m43ng <shinmg2520@gmail.com>
2019-05-27 23:30:14 +0900

    updated response time evaluation

--- evaluate.c
+++ evaluate.c
@@ -1,21 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "evaluate.h"
 
 void print_evaluation(uint num_of_proc, ProcPtr proc_list, ChartPtr chart_ptr){
-    unsigned long long total_turnaround_time=0, total_waiting_time=0;
+    unsigned long long total_turnaround_time=0, total_waiting_time=0, total_response_time=0;
     unsigned int i=0, last_timeline, working_time=0;
-    double average_turnaround_time=0, average_waiting_time=0, throughput=0, cpu_utilization=0;
+    double average_turnaround_time=0, average_waiting_time=0, throughput=0, cpu_utilization=0, average_response_time=0;
+    unsigned int* first_response_time = (unsigned int*)malloc(sizeof(unsigned int) * num_of_proc);
+    memset(first_response_time, 0, sizeof(unsigned int) * num_of_proc);
 
     while(1){
         if(chart_ptr->processes[i] == 0)
             break;
+        if(!first_response_time[chart_ptr->processes[i]-1])
+            first_response_time[chart_ptr->processes[i]-1] = chart_ptr->end[i];
         working_time += chart_ptr->end[i] - chart_ptr->start[i];
         i++;
     }
     last_timeline = chart_ptr->end[i-1];
     throughput = (double) num_of_proc / last_timeline;
     cpu_utilization = (double) working_time / last_timeline * 100;
+
+    for(i=0;i<num_of_proc; i++){
+        total_response_time += first_response_time[i] - proc_list[i].arrival;
+    }
+    average_response_time = (double) total_response_time / num_of_proc;
 
     for(i=0;i<num_of_proc; i++){
         total_turnaround_time += proc_list[i].done_time - proc_list[i].arrival;
@@ -26,5 +37,9 @@ void print_evaluation(uint num_of_proc, ProcPtr proc_list, ChartPtr chart_ptr){
 
     printf("Throughput : %lf / CPU Utilization : %lf%%\n", throughput, cpu_utilization);
     printf("Total / Average Turnaround Time : %lld / %lf\n", total_turnaround_time, average_turnaround_time);
-    printf("Total / Average Waiting Time : %lld / %lf\n\n", total_waiting_time, average_waiting_time);
+    printf("Total / Average Waiting Time : %lld / %lf\n", total_waiting_time, average_waiting_time);
+    printf("Total / Average Response Time : %lld / %lf\n\n", total_response_time, average_response_time);
+
+    // free dynamically allocated variables
+    free(first_response_time);
 }
